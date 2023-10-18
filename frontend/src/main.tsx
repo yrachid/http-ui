17da1c8ef1f041f41f524243e62a762e4395d88e
yrachid <anuar.yasser@gmail.com>
2023-10-18 08:06:53 +0100

    #5: Adds MUI CSS Provider. Enables dark mode.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import './style.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App";
+import { CssVarsProvider } from "@mui/joy";
 
-const container = document.getElementById('root')
+const container = document.getElementById("root");
 
-const root = createRoot(container!)
+const root = createRoot(container!);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <CssVarsProvider defaultMode="dark">
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>
+);
