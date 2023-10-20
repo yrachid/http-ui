fa4fe5286dbfb4f5b606006408a547a47f5f31e3
yrachid <anuar.yasser@gmail.com>
2023-10-20 16:34:41 +0100

    #9: Creates first backend unit tests

---
+++ internal/http/client.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+  "net/http"
+  "io"
+)
+
+type HttpRequest struct {
+  Url string
+  RequestBody string
+}
+
+type HttpClient struct {
+}
+
+func NewHttpClient() *HttpClient {
+  return &HttpClient{}
+}
+
+func (client *HttpClient) Get(request HttpRequest) (string, error) {
+  response, err := http.Get(request.Url)
+
+  if err != nil {
+    return "", err
+  }
+
+  var responseBodyBytes, readError = io.ReadAll(response.Body)
+
+  if readError != nil {
+    return "", readError
+  }
+
+  return string(responseBodyBytes), nil
+}
---
+++ internal/http/client_test.go
@@ -0,0 +1,66 @@
+package client
+
+import "testing"
+
+func TestGetProvidesResponseBodyAsString(t *testing.T) {
+
+  var request = HttpRequest{
+    Url: "http://localhost:8080/success/text",
+  }
+
+  var client = NewHttpClient()
+
+  response, err := client.Get(request)
+
+  if err != nil {
+    t.Errorf("Expected a successful but got error: %s", err.Error())
+  }
+
+  if response != "Hello, world!" {
+    t.Errorf("Expected response body to be 'Hello, world!' but got '%s'", response)
+  }
+}
+
+func TestGetProvidesErrorWhenHostIsInvalid(t *testing.T) {
+    var request = HttpRequest{
+      Url: "http://site.invalid/success/text",
+    }
+
+    var client = NewHttpClient()
+
+    response, err := client.Get(request)
+
+    if response != "" {
+      t.Errorf("Expected response body to be empty but got '%s'", response)
+    }
+
+    if err == nil {
+      t.Errorf("Expected an error but got none")
+    }
+
+    if err.Error() != "Get \"http://site.invalid/success/text\": dial tcp: lookup site.invalid: no such host" {
+      t.Errorf("Unexpected error message: %s", err.Error())
+    }
+}
+
+func TestGetProvidesErrorWhenResponseDataIsInvalid(t *testing.T) {
+  var request = HttpRequest{
+    Url: "http://localhost:8080/fault/malformed-response",
+  }
+
+  var client = NewHttpClient()
+
+  response, err := client.Get(request)
+
+  if response != "" {
+    t.Errorf("Expected response body to be empty but got '%s'", response)
+  }
+
+  if err == nil {
+    t.Errorf("Expected an error but got none")
+  }
+
+  if err.Error() != "unexpected EOF" {
+    t.Errorf("Unexpected error message: %s", err.Error())
+  }
+}
