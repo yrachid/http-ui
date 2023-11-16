ca821729bf5fb72143cdf91398d0087d2078c868
yrachid <anuar.yasser@gmail.com>
2023-11-16 00:03:24 +0000

    #7 simplest logic to implement table-based editing

--- frontend/src/HeaderEditor.test.tsx
+++ frontend/src/HeaderEditor.test.tsx
@@ -2,12 +2,35 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import { HeaderEditor } from "./RequestEditor";
+import { act } from "react-dom/test-utils";
+
+const type = (input: HTMLElement, intendedText: string) =>
+  fireEvent.change(input, { target: { value: intendedText } });
 
 describe("HeaderEditor", () => {
-  it("renders", async () => {
-    render(<HeaderEditor />);
+  it("Inserts a new header on the header list", async () => {
+    await act(() => render(<HeaderEditor />));
 
-    const headerTable = await screen.findByRole("header-table-editor");
+    await act(async () => {
+      const headerNameInput = await screen.findByPlaceholderText("Header name");
+      const headerValueInput = await screen.findByPlaceholderText(
+        "Header value"
+      );
 
+      const insertHeaderButton = await screen.findByRole("insert-header");
+
+      type(headerNameInput, "Content-Type");
+      type(headerValueInput, "application/json");
+
+      fireEvent.click(insertHeaderButton);
+    });
+
+    const insertedHeaderName = await screen.findByRole("inserted-header-name");
+    const insertedHeaderValue = await screen.findByRole(
+      "inserted-header-value"
+    );
+
+    expect(insertedHeaderName).toHaveTextContent("Content-Type");
+    expect(insertedHeaderValue).toHaveTextContent("application/json");
   });
 });
--- frontend/src/RequestEditor.tsx
+++ frontend/src/RequestEditor.tsx
@@ -4,22 +4,80 @@ import TabList from "@mui/joy/TabList";
 import { TabPanel } from "@mui/base";
 import Table from "@mui/joy/Table";
 import Grid from "@mui/joy/Grid";
+import Input from "@mui/joy/Input";
 import "./json-editor.css";
+import { useState } from "react";
+import { Button } from "@mui/joy";
+
+type HttpHeaders = Record<string, string>;
+
+type InserterProps = {
+  onInsert: (name: string, value: string) => void;
+};
+
+const HeaderInserter = (props: InserterProps) => {
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+
+  return (
+    <Grid container>
+      <Grid xs={6}>
+        <Input
+          sx={{ borderRadius: "0px", fontFamily: "monospace" }}
+          role="header-name-inserter"
+          placeholder="Header name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </Grid>
+      <Grid xs={6}>
+        <Input
+          sx={{ borderRadius: "0px", fontFamily: "monospace" }}
+          role="header-value-inserter"
+          placeholder="Header value"
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
+          endDecorator={
+            <Button
+              role="insert-header"
+              onClick={() => props.onInsert(name, value)}
+            >
+              +
+            </Button>
+          }
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
 export const HeaderEditor = () => {
+  const [headers, setHeaders] = useState<HttpHeaders>({});
+
   return (
-    <Table stripe="odd" borderAxis="both" role="header-table-editor">
-      <tbody>
-        <tr>
-          <td>Accept</td>
-          <td>application/json</td>
-        </tr>
-        <tr>
-          <td>Content-Type</td>
-          <td>application/json</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <HeaderInserter
+        onInsert={(name, value) =>
+          setHeaders({
+            ...headers,
+            ...{ [name]: value },
+          })
+        }
+      />
+      <Table stripe="odd" borderAxis="both" role="header-table-editor">
+        <tbody>
+          {Object.keys(headers).map((headerName, id) => (
+            <tr role="inserted-header" key={id}>
+              <td role="inserted-header-name">{headerName}</td>
+              <td role="inserted-header-value">{headers[headerName]}</td>
+              <td>
+                <Button>-</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
