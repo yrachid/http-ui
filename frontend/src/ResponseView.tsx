e0e3919e9eefede02d29824f7a2d451619be6dff
yrachid <anuar.yasser@gmail.com>
2023-12-15 10:19:31 +0000

    #2: Structures ResponseView as a Grid

--- frontend/src/ResponseView.tsx
+++ frontend/src/ResponseView.tsx
@@ -1,3 +1,4 @@
+import Grid from "@mui/joy/Grid";
 import styled from "styled-components";
 
 export type LastResponse =
@@ -5,19 +6,49 @@ export type LastResponse =
   | { successful: true; response: { body: string; statusCode: number } }
   | { successful: false; error: string };
 
-const ResponseViewContainer = styled.div`
-  display: flex;
-`;
+export const ResponseView = (props: { lastResponse: LastResponse }) => {
+  if (props.lastResponse === null) {
+    return <p>Waiting for a request to be made...</p>;
+  }
 
-export const ResponseView = (props: { lastResponse: LastResponse }) => (
-  <ResponseViewContainer>
-    {props.lastResponse?.successful && (
-      <p role="response-status-code">{props.lastResponse.response.statusCode}</p>
-    )}
-    {props.lastResponse?.successful && (
-      <code role="response-body-content">
-        {props.lastResponse.response.body}
-      </code>
-    )}
-  </ResponseViewContainer>
-);
+  if (!props.lastResponse.successful) {
+    return (
+      <p>
+        Something went wrong: {props.lastResponse?.error ?? "Unknown Error"}
+      </p>
+    );
+  }
+
+  const StatusCodeContainer = styled.section`
+    padding: 5px;
+  `;
+
+  const ResponseBodyContainer = styled.section`
+    padding: 5px;
+    max-height: 650px;
+    overflow: scroll;
+  `;
+
+  return (
+    <Grid
+      xs={11}
+      container
+      spacing={0}
+      alignItems="center"
+      sx={{
+        padding: "10px",
+      }}
+    >
+      <Grid xs={12}>
+        <StatusCodeContainer role="response-status-code">
+          {props.lastResponse.response.statusCode}
+        </StatusCodeContainer>
+      </Grid>
+      <Grid xs={12}>
+        <ResponseBodyContainer role="response-body-content">
+          <code>{props.lastResponse.response.body}</code>
+        </ResponseBodyContainer>
+      </Grid>
+    </Grid>
+  );
+};
